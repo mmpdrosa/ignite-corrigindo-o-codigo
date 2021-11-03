b20e2c2779d7dde499d8ae3bb8ff8801ea66508e
mmpdrosa <matheusmp2001@gmail.com>
2021-11-03 17:58:52 -0300

    challenge completed

--- src/index.js
+++ src/index.js
@@ -23,20 +23,28 @@ app.post("/repositories", (request, response) => {
     likes: 0
   };
 
-  return response.json(repository);
+  repositories.push(repository);
+
+  return response.status(201).json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
   const { id } = request.params;
-  const updatedRepository = request.body;
+  const { title, url, techs } = request.body;
 
-  repositoryIndex = repositories.findindex(repository => repository.id === id);
+  repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
   if (repositoryIndex < 0) {
-    return response.status(404).json({ error: "Repository not found" });
+    return response.status(404).json({ error: "Repository not found." });
   }
 
-  const repository = { ...repositories[repositoryIndex], ...updatedRepository };
+  const updatedRepository = {
+    title,
+    url,
+    techs
+  };
+
+  const repository = { ...repositories[repositoryIndex], ...updatedRepository};
 
   repositories[repositoryIndex] = repository;
 
@@ -48,8 +56,8 @@ app.delete("/repositories/:id", (request, response) => {
 
   repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
-  if (repositoryIndex > 0) {
-    return response.status(404).json({ error: "Repository not found" });
+  if (repositoryIndex < 0) {
+    return response.status(404).json({ error: "Repository not found." });
   }
 
   repositories.splice(repositoryIndex, 1);
@@ -63,12 +71,14 @@ app.post("/repositories/:id/like", (request, response) => {
   repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
   if (repositoryIndex < 0) {
-    return response.status(404).json({ error: "Repository not found" });
+    return response.status(404).json({ error: "Repository not found." });
   }
 
-  const likes = ++repositories[repositoryIndex].likes;
+  repositories[repositoryIndex].likes++;
 
-  return response.json('likes');
+  const repository = repositories[repositoryIndex];
+
+  return response.json(repository);
 });
 
 module.exports = app;
